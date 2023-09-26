f618318b6094de197b7fa93802c53c3cd9b5128f
dm94 <dm94@hotmail.es>
2023-09-26 18:58:43 +0200

    feature: Added the meta description and site element schema

--- src/views/components/header.vue
+++ src/views/components/header.vue
@@ -10,24 +10,26 @@ const { t } = useI18n();
       class="relative flex w-full items-center justify-between bg-white py-2 text-neutral-600 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 dark:text-neutral-200 md:flex-wrap md:justify-start"
     >
       <div class="flex w-full flex-wrap items-center justify-between px-3">
-        <ul class="mr-auto flex flex-col lg:flex-row">
-          <li class="lg:mb-0 lg:pr-2">
+        <ul itemscope itemtype="https://schema.org/SiteNavigationElement" class="mr-auto flex flex-col lg:flex-row">
+          <li itemprop="name" class="lg:mb-0 lg:pr-2">
             <router-link
               :to="{ name: RouteName.HOME }"
               data-te-nav-link-ref
               data-te-ripple-init
               data-te-ripple-color="light"
               class="block disabled:text-black/30 dark:text-white lg:p-2"
+              :title="RouteName.HOME"
               >{{ t("header.home") }}</router-link
             >
           </li>
-          <li class="lg:mb-0 lg:pr-2">
+          <li itemprop="name" class="lg:mb-0 lg:pr-2">
             <router-link
               :to="{ name: RouteName.UPLOAD }"
               data-te-nav-link-ref
               data-te-ripple-init
               data-te-ripple-color="light"
               class="block disabled:text-black/30 dark:text-white lg:p-2"
+              :title="RouteName.UPLOAD"
               >{{ t("header.uploadConfig") }}</router-link
             >
           </li>
