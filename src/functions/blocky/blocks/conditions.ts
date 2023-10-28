a513760571e39be4dc32d8018dfbb5ef95bd9e89
Dm94 <dm94@hotmail.es>
2023-10-28 17:08:48 +0200

    feat: oneCondition

--- src/functions/blocky/blocks/conditions.ts
+++ src/functions/blocky/blocks/conditions.ts
@@ -252,3 +252,21 @@ javascriptGenerator.forBlock['numericalCondition'] = function (block: any, gener
 
   return `if(${firstNumber}${operator}${secondNumber})`;
 };
+
+Blockly.Blocks["oneCondition"] = {
+  init: function () {
+    this.appendStatementInput("conditions")
+      .setCheck("Boolean")
+      .appendField("One")
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setOutput(true, 'Boolean');
+    this.setColour(200);
+    this.setTooltip("Returns true if exactly one child condition return true");
+  },
+};
+
+javascriptGenerator.forBlock["oneCondition"] = function (block: any, generator: any) {
+  const conditions = generator.statementToCode(block, "conditions").trim();
+  return `one(${conditions})`;
+};
