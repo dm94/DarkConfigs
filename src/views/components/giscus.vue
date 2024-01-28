c94f315f07e2f2e1f0ed88a295988749db891c9a
dm94 <dm94@hotmail.es>
2024-01-28 14:16:49 +0100

    refactor: Moved gitcus variables to environment variables

--- src/views/components/giscus.vue
+++ src/views/components/giscus.vue
@@ -4,6 +4,11 @@ import { onBeforeMount, ref } from "vue";
 
 const showGitCus = ref(false);
 
+const GISCUS_REPO = import.meta.env.VITE_APP_GISCUS_REPO;
+const GISCUS_REPO_ID = import.meta.env.VITE_APP_GISCUS_REPO_ID;
+const GISCUS_CATEGORY = import.meta.env.VITE_APP_GISCUS_CATEGORY;
+const GISCUS_CATEGORY_ID = import.meta.env.VITE_APP_GISCUS_CATEGORY_ID;
+
 onBeforeMount(() => {
   if (localStorage.getItem("accept-cookies")) {
     showGitCus.value = true;
@@ -13,20 +18,23 @@ onBeforeMount(() => {
 </script>
 
 <template>
-  <div  v-if="showGitCus" class="flex container mx-auto p-2 gap-4 flex-col bg-neutral-400 max-w-xl rounded-lg border border-neutral-700">
+  <div
+    v-if="showGitCus"
+    class="flex container mx-auto p-2 gap-4 flex-col bg-neutral-400 max-w-xl rounded-lg border border-neutral-700"
+  >
     <Giscus
-    id="comments"
-    repo="dm94/DarkConfigs"
-    repo-id="R_kgDOKPybJw"
-    category="Configs"
-    category-id="DIC_kwDOKPybJ84Ccv7p"
-    mapping="url"
-    reactions-enabled="1"
-    emit-metadata="0"
-    input-position="top"
-    theme="dark_tritanopia"
-    lang="en"
-    loading="lazy"
-  />
-    </div>
+      id="comments"
+      :repo="GISCUS_REPO"
+      :repo-id="GISCUS_REPO_ID"
+      :category="GISCUS_CATEGORY"
+      :category-id="GISCUS_CATEGORY_ID"
+      mapping="url"
+      reactions-enabled="1"
+      emit-metadata="0"
+      input-position="top"
+      theme="dark_tritanopia"
+      lang="en"
+      loading="lazy"
+    />
+  </div>
 </template>
